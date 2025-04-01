46e8c12094275a98dfb5654572f154de4c483a87
abdlmd <abdulmueedhabibraja@gmail.com>
2025-04-01 19:39:18 +0500

    Web App Completed

--- src/components/Habit.jsx
+++ src/components/Habit.jsx
@@ -3,21 +3,42 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { ImRadioUnchecked } from "react-icons/im";
 
-function Habit({ name }) {
+function Habit({ id, name, streak, completed, fetchHabit }) {
+  function handleChange(id) {
+    fetch(`http://localhost:8080/api/habit/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => fetchHabit());
+  }
+
+  function deleteTask(id) {
+    fetch(`http://localhost:8080/api/habit/${id}`, {
+      method: "DELETE",
+    }).then(() => fetchHabit());
+  }
   return (
-    <div className="text-xl  inline-flex gap-3 pb-3 pt-2 px-3 bg-orange-500 rounded-4xl w-auto">
-      <button className="cursor-pointer">
-        <RiDeleteBin6Line className="fill-white mt-1.5" />
-      </button>
-      <p>{name}</p>? (
-      <button className="mt-1.5 cursor-pointer">
-        <FaRegCheckCircle />
-      </button>
-      ) : (
-      <button className="mt-1.5 cursor-pointer">
-        <ImRadioUnchecked />
-      </button>
-      )
+    <div className="text-xl flex flex-col gap-0.5  px-6 w-fit bg-orange-500 rounded-4xl ">
+      <div className="flex gap-3">
+        <button onClick={() => deleteTask(id)} className="cursor-pointer">
+          <RiDeleteBin6Line className="fill-white mt-1.5" />
+        </button>
+        <p className="mt-2">{name}</p>
+        {completed ? (
+          <button className="mt-1.5 cursor-pointer">
+            <FaRegCheckCircle />
+          </button>
+        ) : (
+          <button
+            className="mt-1.5 cursor-pointer"
+            onClick={() => handleChange(id)}
+          >
+            <ImRadioUnchecked />
+          </button>
+        )}
+      </div>
+      <p className="text-sm ml-4 ">Streak:{streak}</p>
     </div>
   );
 }
--- src/pages/HabitLayout.jsx
+++ src/pages/HabitLayout.jsx
@@ -5,6 +5,8 @@ import Habit from "../components/Habit.jsx";
 
 function HabitLayout() {
   const [data, setData] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [text, setText] = useState("");
 
   async function fetchHabits() {
     const res = await fetch("http://localhost:8080/api/habit");
@@ -16,6 +18,22 @@ function HabitLayout() {
   useEffect(() => {
     fetchHabits();
   }, []);
+  function handleClick() {
+    setShowForm(true);
+  }
+  function handleSubmit() {
+    event.preventDefault();
+    if (!text) return;
+    const newHabit = { name: text, completedToday: false, streakDays: 0 };
+    fetch("http://localhost:8080/api/habit", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(newHabit),
+    })
+      .then(() => setShowForm(false))
+      .then(() => fetchHabits())
+      .then(() => setText(""));
+  }
   return (
     <div className="bg-gray-900 h-screen text-white font-mono">
       <div className="pl-40 pt-20">
@@ -37,10 +55,41 @@ function HabitLayout() {
         <ul className="mt-2">
           {data.map((d) => (
             <li className="mb-4">
-              <Habit name={d.name} />
+              <Habit
+                name={d.name}
+                streak={d.streakDays}
+                completed={d.completedToday}
+                id={d.id}
+                fetchHabit={() => fetchHabits()}
+              />
             </li>
           ))}
         </ul>
+        {!showForm ? (
+          <button
+            className="text-2xl pb-1.5 cursor-pointer bg-orange-500 rounded-full w-10 h-10"
+            onClick={() => handleClick()}
+          >
+            +
+          </button>
+        ) : (
+          <form className="flex flex-col gap-5">
+            <label className="ml-2 uppercase font-semibold text-xl">
+              Enter name of the new Habit
+            </label>
+            <input
+              className="bg-white rounded-2xl pl-2 text-black h-8 w-100"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              className="cursor-pointer bg-orange-500 rounded-2xl w-20 p-2"
+              onClick={() => handleSubmit()}
+            >
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
